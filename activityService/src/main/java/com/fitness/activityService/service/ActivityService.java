d1393a2fe2edd3003426e01851b1ed9eacd47d61
Artur <artur.helta@onet.pl>
2025-05-26 18:51:39 +0200

    add RabbitMq producer to activityService

--- activityService/src/main/java/com/fitness/activityService/service/ActivityService.java
+++ activityService/src/main/java/com/fitness/activityService/service/ActivityService.java
@@ -5,16 +5,26 @@ import com.fitness.activityService.dto.ActivityResponseDto;
 import com.fitness.activityService.model.Activity;
 import com.fitness.activityService.repository.ActivityRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ActivityService {
 
     private final ActivityRepository activityRepository;
     private final UserValidationService userValidationService;
+    private final RabbitTemplate rabbitTemplate;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
 
     public ActivityResponseDto trackActivity(ActivityRequestDto activityRequestDto){
         if (!userValidationService.validateUser(activityRequestDto.userId())){
@@ -30,6 +40,13 @@ public class ActivityService {
                 .build();
 
         Activity savedActivity = activityRepository.save(activity);
+
+        try {
+            rabbitTemplate.convertAndSend(exchange, routingKey, savedActivity);
+        } catch(Exception e) {
+            log.error("Failed to publish activity to RabbitMq: ", e);
+        }
+
         return mapToResponseDto(savedActivity);
     }
 
---
+++ aiservice/src/main/java/com/fitness/aiservice/service/RecommendationService.java
@@ -0,0 +1,24 @@
+package com.fitness.aiservice.service;
+
+import com.fitness.aiservice.model.Recommendation;
+import com.fitness.aiservice.repository.RecommendationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class RecommendationService {
+    private final RecommendationRepository recommendationRepository;
+
+    public List<Recommendation> getUserRecommendation(UUID userId) {
+        return recommendationRepository.findByUserId(userId);
+    }
+
+    public Recommendation getActivityRecommendation(String activityId) {
+        return recommendationRepository.findByActivityId(activityId)
+                .orElseThrow(() -> new RuntimeException("Recommendation not found for activity: " + activityId));
+    }
+}
